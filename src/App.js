12fd59538fca87470f6c7451805f5adf1d148be9
Jexeones24 <jexeones@gmail.com>
2017-11-04 21:25:30 -0400

    fetch sets state to populate table for one city

--- src/App.js
+++ src/App.js
@@ -3,15 +3,43 @@ import './App.css'
 import { Home, Forecast, Footer } from './components/weather'
 
 class App extends Component {
+  state = {
+    titles: ['CITY', 'COUNTRY', 'AVG. TEMP.', 'HIGH', 'LOW'],
+    // details: [
+    //   {
+    //     'city': '',
+    //     'country': '',
+    //     'avgTemp': null,
+    //     'high': null,
+    //     'low': null
+    //   }
+    // ]
+    details: []
+  }
+
+  componentDidMount () {
+    return fetch('http://api.openweathermap.org/data/2.5/weather?q=detroitid=524901&APPID=bf8ca4717bdd97a9ba7800e9f63c7b72')
+      .then(resp => resp.json())
+      .then(data => {
+        let details = this.state.details
+        details.city = data.name
+        details.country = data.sys.country
+        details.avgTemp = data.main.temp
+        details.high = data.main.temp_max
+        details.low = data.main.temp_min
+        this.setState({ details: [...this.state.details, details] }, () => { console.log('details:', this.state.details, 'titles:', this.state.titles) })
+      })
+    }
+
   render () {
     return (
       <div className='App'>
         <div className='App-header'>
-          <img src="" alt=""/>
+          <img src='' alt=''/>
           <h2>WEATHER APP</h2>
         </div>
         <div className='weather-app'>
-          <Home />
+          <Home titles={this.state.titles} details={this.state.details} />
           <Footer />
         </div>
       </div>
--- src/components/weather/Home.js
+++ src/components/weather/Home.js
@@ -1,47 +1,23 @@
 import React, { Component } from 'react'
 
 export class Home extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      titles: ['CITY', 'COUNTRY', 'AVG. TEMP.', 'HIGH', 'LOW'],
-      details: []
-    }
+  render () {
+    return (
+      <div className='home'>
+        <table className='table'>
+          <thead>
+            <tr>
+              {this.props.titles.map(title => <Header key={title} title={title} />)}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.details.map((detail, idx) => <Row key={idx} detail={detail} />)}
+          </tbody>
+        </table>
+      </div>
+    )
   }
-
-  componentDidMount () {
-    return fetch('http://api.openweathermap.org/data/2.5/weather?q=detroitid=524901&APPID=bf8ca4717bdd97a9ba7800e9f63c7b72')
-      .then(resp => resp.json())
-      .then(data => {
-        let details = this.state.details
-        details.city = data.name
-        details.country = data.sys.country
-        details.avgTemp = data.main.temp
-        details.high = data.main.temp_max
-        details.low = data.main.temp_min
-        this.setState({ details }, () => { console.log('details:', this.state.details, 'titles:', this.state.titles) })
-      })
-    }
-
-    render(){
-      return (
-        <div className='home'>
-          <table className='table'>
-            <thead>
-              <tr>
-                {this.state.titles.map(title => <Header key={title} title={title} />)}
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.details.map((detail, idx) => <Row key={idx} detail={detail} />)}
-            </tbody>
-          </table>
-        </div>
-      )
-    }
-  }
-
+}
 
 const Header = ({title}) =>
   <th>{title}</th>
